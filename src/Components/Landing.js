758573f8451e3d00fa19271fccd82482df4599f1
Greg Ogun <gregogun97@gmail.com>
2021-12-08 23:49:32 +0000

    feat: add cards and testimonial section

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import { features, testimonials } from '../data';
+import Internet from '../assets/Internet'
+import Newspaper from '../assets/Newspaper'
+import Wishlist from '../assets/Wishlist'
+import Streaming from '../assets/Streaming'
 
 /*
 Hi Brian, I have this set up now so the landing page will load first. I just made a toggle button for us to use to go back and forth from the landing page to the search page when we are working on the app, you can do whatever you want with it. I have an index.css file but feel free to create your own style file for the landing page if you'd like. Don't hesistate to reach out with any questions, just remember since its React there has to be one div/container in the return that houses all of the other elements (I just made a div and set to 100vh) and that you have to use 'className' instead of 'class'.
 
 */
 
+const Features = () => (
+    <div className="features" style={{ display: 'flex', gap: '2rem', minHeight: '256px', color: 'white'}}>
+        {features.map((item) => (
+            <div style={{ display: 'flex', flexDirection: 'column',  backgroundColor: item.color, padding: '2rem', alignItems: 'center' }}>
+                {item.icon === 'internet' && <Internet />}
+                {item.icon === 'wishlist' && <Wishlist />}
+                {item.icon === 'streaming' && <Streaming />}
+                {item.icon === 'newspaper' && <Newspaper />}
+                <h3 style={{ fontSize: '1.5rem', textAlign: 'center' }}>{item.title.toUpperCase()}</h3>
+                <hr style={{ height: '2px', width: '80%', backgroundColor: 'white' }} />
+                <p>{item.description}</p>
+            </div>
+        ))}
+    </div>
+);
 
+const Testimonials = () => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '4rem', backgroundColor: 'lightgray', padding: '3rem 2rem'}}>
+    {testimonials.map((item) => (
+        <div style={{ display: 'flex', gap: '0.5rem', flexDirection: item.reversed ? 'row-reverse' : 'row' }}>
+            <img src={item.imgSrc} alt={item.name} style={{ borderRadius: '9999px', width: '6rem', height: '6rem', backgroundColor: 'floralwhite' }} />
+            <div>
+            <p style={{ margin: 0 }}>{item.testimony}</p>
+            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>{item.name}, {item.location}</p>
+            </div>
+        </div>
+    ))}
+    </div>
+)
 
 
 export default function Landing({home, setHome}) {
-
-
-
-
     return (
         <div style={{height: "100vh"}}>
             Welcome to the landing Page!
-         <button onClick={() => setHome(2)}>Start your search</button>   
+         <button onClick={() => setHome(2)}>Start your search</button>  
+         <div style={{ display: 'flex', flexDirection: 'column', gap: '4rem'}}>
+         <Features /> 
+         <Testimonials />
+         </div>
         </div>
     )
 }
---
+++ src/data/index.js
@@ -0,0 +1,63 @@
+export const features = [
+    {
+
+        color: '#FC636B',
+        icon: 'wishlist',
+        title: 'my wishlist',
+        description:     ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, incidunt vitae iusto in, temporibus expedita at, unde ipsa harum doloremque quo adipisci culpa! Rerum quisquam iste eum illo id? Qui, dolor? Id, totam. Ut sint dolorum eaque mollitia magni aut similique suscipit nesciunt optio animi sapiente quasi, modi molestiae? Itaque.'
+   ,
+    },
+    {
+
+        color: '#484A6F',
+        icon: 'internet',
+        title: 'movie search',
+        description:         ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, incidunt vitae iusto in, temporibus expedita at, unde ipsa harum doloremque quo adipisci culpa! Rerum quisquam iste eum illo id? Qui, dolor? Id, totam. Ut sint dolorum eaque mollitia magni aut similique suscipit nesciunt optio animi sapiente quasi, modi molestiae? Itaque.',
+    },
+    {
+
+        color: '#1AAFD0',
+        icon: 'streaming',
+        title: 'streaming services',
+        description:     ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, incidunt vitae iusto in, temporibus expedita at, unde ipsa harum doloremque quo adipisci culpa! Rerum quisquam iste eum illo id? Qui, dolor? Id, totam. Ut sint dolorum eaque mollitia magni aut similique suscipit nesciunt optio animi sapiente quasi, modi molestiae? Itaque.',
+    },
+    {
+
+        color: '#FFB902',
+        icon: 'newspaper',
+        title: 'movie news',
+        description:     ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, incidunt vitae iusto in, temporibus expedita at, unde ipsa harum doloremque quo adipisci culpa! Rerum quisquam iste eum illo id? Qui, dolor? Id, totam. Ut sint dolorum eaque mollitia magni aut similique suscipit nesciunt optio animi sapiente quasi, modi molestiae? Itaque.',
+    },
+]
+
+
+export const testimonials = [
+    {        
+        reversed: false,
+        imgSrc: '/images/tasha.jpg',
+        name: 'Tasha J',
+        location: 'Chicago, Illinois, US',
+        testimony: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum quae ducimus vel quod at illo recusandae, nobis eos neque nemo repellat eaque non in dicta ea doloribus ad iusto tempore? Nemo sequi error ex quos, expedita mollitia molestiae nihil sed eligendi porro magnam, ad voluptatem. Ut blanditiis accusamus odio? Neque iusto accusamus voluptatibus illum ullam nam hic veniam tempora vitae.',
+    },
+    {       
+        reversed: true,
+        imgSrc: '/images/santiago.jpg',
+        name: 'Santiago C',
+        location: 'Buenos Aires, Argentina',
+        testimony: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum quae ducimus vel quod at illo recusandae, nobis eos neque nemo repellat eaque non in dicta ea doloribus ad iusto tempore? Nemo sequi error ex quos, expedita mollitia molestiae nihil sed eligendi porro magnam, ad voluptatem. Ut blanditiis accusamus odio? Neque iusto accusamus voluptatibus illum ullam nam hic veniam tempora vitae.',
+    },
+    {        
+        reversed: false,
+        imgSrc: '/images/rachel.jpg',
+        name: 'Rachel K',
+        location: 'Perth, Australia',
+        testimony:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum quae ducimus vel quod at illo recusandae, nobis eos neque nemo repellat eaque non in dicta ea doloribus ad iusto tempore? Nemo sequi error ex quos, expedita mollitia molestiae nihil sed eligendi porro magnam, ad voluptatem. Ut blanditiis accusamus odio? Neque iusto accusamus voluptatibus illum ullam nam hic veniam tempora vitae.',
+    },
+    {       
+         reversed: true,
+        imgSrc: '/images/eric.jpg',
+        name: 'Eric D',
+        location: 'London, England',
+        testimony:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum quae ducimus vel quod at illo recusandae, nobis eos neque nemo repellat eaque non in dicta ea doloribus ad iusto tempore? Nemo sequi error ex quos, expedita mollitia molestiae nihil sed eligendi porro magnam, ad voluptatem. Ut blanditiis accusamus odio? Neque iusto accusamus voluptatibus illum ullam nam hic veniam tempora vitae.',
+    },
+]
